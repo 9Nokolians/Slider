7fae40b4a9071947f3a00e02fd152330622d06a1
Nokolians <201000322e@eastdelta.edu.bd>
2021-07-23 16:49:58 +0600

    all added

---
+++ slider.js
@@ -0,0 +1,39 @@
+var images=[
+    'http:/www.sololearn.com/uploads/slider/1.jpg',
+    'http:/www.sololearn.com/uploads/slider/2.jpg',
+    'http:/www.sololearn.com/uploads/slider/3.jpg'
+];
+var num=0;
+var prev=document.getElementById('prev');
+var next=document.getElementById('next');
+alert("Hey welcome");
+prev.addEventListener("click",function(){
+  
+    prevImage();
+
+});
+next.addEventListener("click",function()
+{
+
+    nextImage();
+});
+
+function nextImage() {
+    var slider=document.getElementById('slider');
+    num++;
+    if(num>=images.length)//3
+    {
+        num=0;
+    }
+    slider.src=images[num];
+}
+function prevImage() {
+    var slider=document.getElementById('slider');
+    num--;
+    if(num<0)
+    {
+        num=images.length-1;
+    }
+    slider.src=images[num];
+ 
+}
